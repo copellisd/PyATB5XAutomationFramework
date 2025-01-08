3c6e75631267084e0ddbf62cd059030cac8e378b
copellisd <sruthi.spr13@gmail.com>
2025-01-07 19:58:17 -0600

    2nd jan 2025

---
+++ src/tests/test_sample.py
@@ -0,0 +1,7 @@
+import pytest
+import allure
+@allure.title("sample testcases")
+def test_sample():
+    assert True==True
+
+    
--- src/tests/test_sample_2.py
+++
@@ -1,5 +0,0 @@
-import pytest
-import allure
-@allure.title("sample testcases")
-def test_sample():
-    assert True==True
